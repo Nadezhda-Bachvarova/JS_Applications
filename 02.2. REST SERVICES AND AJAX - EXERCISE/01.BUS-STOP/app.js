cbad50163c12e0da6a33c077f5f858a7cb396cd7
Nadezhda-Mihaylova <64796417+Nadezhda-Mihaylova@users.noreply.github.com>
2020-07-15 16:39:25 +0300

    Update app.js

--- 02.2. REST SERVICES AND AJAX - EXERCISE/01.BUS-STOP/app.js
+++ 02.2. REST SERVICES AND AJAX - EXERCISE/01.BUS-STOP/app.js
@@ -1,3 +1,33 @@
 function getInfo() {
-    console.log("TODO...");
+    const baseURL = 'https://judgetests.firebaseio.com/businfo/{stopId}.json';
+    const validIds = ['1287', '1308', '1327', '2334'];
+
+    const elements = {
+        stopId() { return document.querySelector('input#stopId') },
+        stopName() { return document.querySelector('div#stopName')},
+        buses() { return document.querySelector('ul#buses') }
+    };
+
+    const stopId = elements.stopId().value;
+
+    if (!validIds.includes(stopId)) {
+        elements.stopName().textContent = 'ERROR';
+        return;
+    }
+
+    const url = baseURL.replace('{stopId}', stopId);
+
+    fetch(url)
+        .then((responce) => responce.json())
+        .then((result) => showInfo(result));
+
+    function showInfo(data) {
+        elements.stopName().textContent = data.name;
+
+        Object.keys(data.buses).forEach((bus) => {
+            let listItem = document.createElement('li');
+            listItem.textContent = `Bus ${bus} arrives in ${data.buses[bus]}`;
+            elements.buses().appendChild(listItem);
+        });
+    }
 }
